14edbadce61900a4c3613ac27cbe1635f0aad190
EddyVinck <eddyvinck95@gmail.com>
2022-08-09 17:26:01 +0200

    rename files

---
+++ app/utils/prismicio.server.ts
@@ -0,0 +1,29 @@
+import LRU from "lru-cache";
+import { getPrismicClient } from "./prismicio";
+import type { PrismicDocument } from "@prismicio/types";
+
+const options = {
+  max: 500,
+};
+
+export const prismicCache = new LRU(options);
+
+export function addPrismicDocToCache<Tdocument extends PrismicDocument>(
+  uid: string,
+  doc: Tdocument
+) {
+  prismicCache.set(uid, doc);
+}
+
+export function getCachedDataByUID(
+  customType: string,
+  uid: string
+): Promise<PrismicDocument> {
+  const client = getPrismicClient();
+  const doc = prismicCache.get<PrismicDocument>(uid);
+  if (typeof doc !== "undefined") {
+    return Promise.resolve(doc);
+  }
+
+  return client.getByUID(customType, uid);
+}
